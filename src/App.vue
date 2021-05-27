1736e680567550ad6645d73aba98f6e338ac5faa
gatortarheel <gatortarheel@gmail.com>
2021-05-27 07:29:37 -0400

    attempt #1

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+
+    <employee-table />
+  </div>
 </template>
 
 <script>
-export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
+  import EmployeeTable from '@/components/EmployeeTable.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      EmployeeTable,
+    },
   }
-};
 </script>
+
+<style>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
+</style>
